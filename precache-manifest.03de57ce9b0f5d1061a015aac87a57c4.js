220a04f6ab8f64963af53c800d6f230e63b59b70
Megan H <megannh6349@gmail.com>
2020-08-24 00:57:46 -0400

    Updates

---
+++ precache-manifest.03de57ce9b0f5d1061a015aac87a57c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4e149ee5b96ff3f3e66ec9b6b08429",
+    "url": "/Homework-19/index.html"
+  },
+  {
+    "revision": "04d056c56a71b16fa115",
+    "url": "/Homework-19/static/css/main.b9dc26ac.chunk.css"
+  },
+  {
+    "revision": "1a00cbad372db179101e",
+    "url": "/Homework-19/static/js/2.86742283.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Homework-19/static/js/2.86742283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d056c56a71b16fa115",
+    "url": "/Homework-19/static/js/main.0e3ddefc.chunk.js"
+  },
+  {
+    "revision": "8ba8886075f07223bbce",
+    "url": "/Homework-19/static/js/runtime-main.11965edf.js"
+  }
+]);
